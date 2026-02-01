6bded2a38fd8cadd9808258760214c1b058b18c2
Welinton Azevedo <welintonz226@gmail.com>
2026-02-01 20:40:33 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,9 +1,10 @@
 package model.dao;
 
+import db.DB;
 import model.dao.Impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 }
